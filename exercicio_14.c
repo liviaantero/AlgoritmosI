a811c0c67dc5fd31a6c2596d8a9afd8d793e9b0c
Liviaantero <liviaantero96@gmail.com>
2025-04-23 01:10:50 -0400

    Add files via upload

---
+++ exercicio_14.c
@@ -0,0 +1,12 @@
+/*como pedia no exercicio eu declarei numeros reais e caracteres*/
+#include<stdio.h>
+//14- faça um algoritmo que declare dois valores reais e cinco carácteres
+int main(){
+  float a,b;
+  char um[10];
+  char dois[10];
+  char tres[10];
+  char quatro[10];
+  char cinco[10];
+  return(0);
+}
