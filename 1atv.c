ce0105ad1f412727401bd4b5f152d52767484127
Liviaantero <liviaantero96@gmail.com>
2025-04-23 01:10:06 -0400

    Add files via upload

---
+++ 1atv.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+//1:considere um algoritmo que tenha entrada: em um plano cartesiano
+  float x = 2.3;
+  float y = 3.4;
+//2:considere um algoritmo que processe 3 numeros inteiros chamados de a,b,c
+  int a,b,c;
+//3:considere um algoritmo que pegue como entradas a idade de uma pessoa expressa em dias, meses e anos
+  int idade = 19;
+  int mes = 6;
+  int dia = 19;
+//4:considere um algoritmo que tenha como entrada a idade de uma pessoa expressa em dias
+  int idade = 19;
+  int dias = 19;
+//5:considere um algoritmo que tenha como entrada tres notas de um aluno
+  float n1 = 8.5;
+  float n2 = 9.5;
+  float n3 = 3.5;
+//6:considere um algoritmo que leia tres valores reais
+  float a,b,c;
+//7:considere um algoritmo que tenha como entrada dois valores inteiros
+  int a = 2;
+  int b = 3;
+//8:considere um algoritmo que tenha como entrada: codigo do estudante, e suas tres notas
+  int cod = 65167;
+  float cn1 = 0.5;
+  float cn2 = 3.8;
+  float cn3 = 6.0;
+//9:considere um algoritmo que tenha como entrada a altura e o genero de uma pessoa
+  float altura = 1,57;
+  char ge = f;
+//10:considere um algoritmo que tenha como entrada a hora de inicio e a hora de fim do jogo
+  float cj = 14,30;
+  float fj = 01,45;
+//11:considere um algoritmo que tenha como entrada quatro numeros inteiros
+  int a[4] = {1,2,3,4};
+//12:considere um algoritmo que tenha como entrada um valor real
+  float a = 24.5;
+//13:considere um algoritmo que tenha como entrada 10 numeros inteiros
+  int a[10] = {1,2,3,4,5,6,7,8,9,10};
+//14:considere um algoritmo que tenha como entrada quatro caracteres
+  char a[4] = {l,i,v,i,a};
+//15:considere um algoritmo que tenha como entrada dois numeros inteiros, 1 real, tres caracteres
+  int a[2] = {2,3};
+  float b = 2.6;
+  char c[3] = {a,b,c};
+//16 a 21: considere as questao anteriores como exemplo proponha 6 novos algoritmos
