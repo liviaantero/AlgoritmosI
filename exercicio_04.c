57460bd4e39251d699a7d2d171cc4702d14f6f75
Liviaantero <liviaantero96@gmail.com>
2025-04-23 01:54:40 -0400

    Add files via upload

---
+++ exercicio_04.c
@@ -0,0 +1,24 @@
+/*pedi os anos ao usuario e depois calculei se o ano era divisivel por 4 sem deixar resto, se fosso ele seria ano bissexto,
+se nao ele nao seria bissexto*/
+#include<stdio.h>
+/*4- faça um algoritmo que receba duas datas em anos cuja a primeira entrada tenha um valor inteiro menor que o segundo
+ valor, o algoritmo deve mostrar os anos bissextos dentro do intervalo das duas entradas*/
+int main()
+{
+  int ano1;
+  int ano2;
+  printf("digite o ano do seu nascimento:");
+  scanf("%d",&ano1);
+  printf("digite o ano atual:");
+  scanf("%d",&ano2);
+  for(int i = ano1; i <= ano2; i++) {
+    if(i % 4 == 0) {
+      printf("e ano bissexto:%d\n",i);
+    }
+    else {
+      printf("nao e bissexto:%d\n",i);
+    }
+  }
+
+  return(0);
+}
