c23ff3f559175da2f29dcc7f7643b9db646e49b5
Liviaantero <liviaantero96@gmail.com>
2025-04-23 01:39:48 -0400

    Add files via upload

---
+++ exercicio_06.c
@@ -0,0 +1,16 @@
+/*pedi todos os numeros ao usuario fiz o calculo da soma e mostrei o resultado na tela*/
+#include<stdio.h>
+//6- algoritmo que tenha entrada dois números inteiros que some e depois mostre o resultado
+int main(){
+  int a;
+  int b;
+  int r;
+  printf("insira o primeiro numero:");
+  scanf("%d", &a);
+  printf("insira o segundo numero:");
+  scanf("%d", &b);
+  r = a + b;
+  printf("resultado = %d",r);
+  return (0);
+}
+
